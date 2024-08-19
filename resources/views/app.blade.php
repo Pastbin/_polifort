be189eb52d38c5a021a9818c4fcf960ecc8ea5c5
Pastbin Alex <pastbin.alex@gmail.com>
2024-08-19 17:54:04 +0300

    SEO meta

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,9 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta title="ПолифортМаш - Металлообработка в Самаре">
+  <meta name="keywords"
+    content="металлообработка, заказы на металлообработку, металлообработка чпу, завод металлообработки, машиностроение и металлообработка, цех металлообработки">
   <title inertia>ПолифортМаш - Металлообработка</title>
   @routes
   @vite(["resources/js/app.ts", "resources/js/Pages/{$page["component"]}.vue"])
