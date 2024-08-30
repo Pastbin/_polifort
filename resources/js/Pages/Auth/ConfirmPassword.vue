1b287cbc8df84b4de95422fe57585264d7c7581b
pastbin <pastbin.alex@gmail.com>
2024-08-30 23:35:37 +0300

    fix TS error and components

--- resources/js/Pages/Auth/ConfirmPassword.vue
+++ resources/js/Pages/Auth/ConfirmPassword.vue
@@ -1,8 +1,4 @@
 <script setup lang="ts">
-import InputError from "@/Components/InputError.vue";
-import InputLabel from "@/Components/InputLabel.vue";
-import PrimaryButton from "@/Components/PrimaryButton.vue";
-import TextInput from "@/Components/TextInput.vue";
 import { Head, useForm } from "@inertiajs/vue3";
 
 const form = useForm({
@@ -23,28 +19,19 @@ const submit = () => {
     <Head title="Confirm Password" />
 
     <div class="mb-4 text-sm text-gray-600">
-      This is a secure area of the application. Please confirm your password before continuing.
+      Это безопасная область приложения. Пожалуйста, подтвердите свой пароль, прежде чем продолжить.
     </div>
 
     <form @submit.prevent="submit">
       <div>
-        <InputLabel for="password" value="Password" />
-        <TextInput
-          id="password"
-          type="password"
-          class="mt-1 block w-full"
-          v-model="form.password"
-          required
-          autocomplete="current-password"
-          autofocus
-        />
-        <InputError class="mt-2" :message="form.errors.password" />
+        <label>
+          <input type="password" v-model="form.password" autofocus required />
+        </label>
+        <div class="form-text">{{ form.errors.password }}</div>
       </div>
 
       <div class="flex justify-end mt-4">
-        <PrimaryButton class="ms-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-          Confirm
-        </PrimaryButton>
+        <button type="submit" class="btn btn-primary" :disabled="form.processing">Подтвердить</button>
       </div>
     </form>
   </div>
--- resources/js/Pages/Auth/VerifyEmail.vue
+++ resources/js/Pages/Auth/VerifyEmail.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { computed } from "vue";
-import PrimaryButton from "@/Components/PrimaryButton.vue";
 import { Head, Link, useForm } from "@inertiajs/vue3";
 
 const props = defineProps<{
@@ -21,27 +20,22 @@ const verificationLinkSent = computed(() => props.status === "verification-link-
     <Head title="Email Verification" />
 
     <div class="mb-4 text-sm text-gray-600">
-      Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just
-      emailed to you? If you didn't receive the email, we will gladly send you another.
+      Спасибо за регистрацию! Прежде чем начать, не могли бы вы подтвердить свой адрес электронной почты, нажав на
+      ссылку, которую мы только что отправил вам по электронной почте? Если вы не получили письмо, мы с радостью
+      отправим вам другое.
     </div>
 
     <div class="mb-4 font-medium text-sm text-green-600" v-if="verificationLinkSent">
-      A new verification link has been sent to the email address you provided during registration.
+      Новая ссылка для подтверждения была отправлена ​​на адрес электронной почты, который вы указали при регистрации.
     </div>
 
     <form @submit.prevent="submit">
       <div class="mt-4 flex items-center justify-between">
-        <PrimaryButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-          Resend Verification Email
-        </PrimaryButton>
+        <button type="submit" class="btn btn-primary" :disabled="form.processing">
+          Повторно отправить письмо с подтверждением
+        </button>
 
-        <Link
-          :href="route('logout')"
-          method="post"
-          as="button"
-          class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-          >Log Out</Link
-        >
+        <Link :href="route('logout')" method="post" as="button">Выйти</Link>
       </div>
     </form>
   </div>
